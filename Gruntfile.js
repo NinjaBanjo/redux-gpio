d9cab0bd1a64531738782c6840001085270abfca
Derek Persons <derek.persons@gmail.com>
2016-03-13 00:21:32 -0600

    stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,30 @@ var webpackConfig = require('./webpack.config')();
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    mochaTest: {
+      all: {
+        src: ['tests/unit/**/*-test.js'],
+        options: {
+          reporter: 'dot',
+          require: [
+            'babel-register',
+            'coverage/blanket'
+          ]
+        },
+        coverage: {
+          options: {
+            reporter: 'html-cov',
+            quiet: true,
+            captureFile: 'coverage/coverage.html'
+          },
+          src: ['tests/**/*.js']
+        }
+      },
+      most: {
+        src: ['tests/**/!(*-s-test)-test.js'],
+        reporter: 'dot'
+      }
+    },
     webpack: {
       main: webpackConfigProd
     }
@@ -14,8 +38,11 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('test', [
-    'cleam:coverage',
-    'karma:unit'
+    'mochaTest:all'
+  ]);
+
+  grunt.registerTask('testmost', [
+    'mochaTest:most'
   ]);
 
   grunt.registerTask('default', [
@@ -23,4 +50,5 @@ module.exports = function(grunt) {
   ]);
 
   grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-mocha-test');
 };
