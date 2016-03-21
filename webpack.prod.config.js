1fdd786c8fcc7fa613de8ee7f03694fffa755d03
Derek Persons <derek.persons@gmail.com>
2016-03-21 00:55:44 -0500

    fixing types

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,6 +6,6 @@ webpackConfig.devtool = 'source-map';
 webpack.plugins = webpackConfig.plugins.concat([
   new webpack.optimize.DedupePlugin(),
   new webpack.BannerPlugin(`BuildVersion: ${pkg.version}\nBuildTime: ${new Date().toString()}`)
-])
+]);
 
 module.exports = webpackConfig;
