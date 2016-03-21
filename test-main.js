21f7859895fd2598a7323aa571079b31526650b1
Derek Persons <derek.persons@gmail.com>
2016-03-20 21:49:54 -0500

    more changes

--- test-main.js
+++ test-main.js
@@ -1,7 +1,12 @@
+// setup test env
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
-
+// Include tests with require.context
 var unitTests = require.context("./lib", true, /-test\.js$/);
 var integrationTests = require.context("./tests/integration", true, /-test\.js$/);
 
+// Run the tests
 unitTests.keys().forEach(unitTests);
 integrationTests.keys().forEach(integrationTests);
