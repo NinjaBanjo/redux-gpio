ea4d3ea09f54289f45791150679e37ea25c74694
that guy <me@somewherE>
2016-04-09 23:39:33 +0000

    it works?

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var webpackConfig = require('./webpack.config')();
 var pkg = require('./package.json');
 
-webpackConfig.devtool = 'source-map';
+//webpackConfig.devtool = 'source-map';
 webpackConfig.plugins = [
   new webpack.optimize.DedupePlugin(),
   new webpack.BannerPlugin(`BuildVersion: ${pkg.version}\nBuildTime: ${new Date().toString()}`)
